15e5c01008eb9f55962f994fe6cac248c8a263c9
Szymon Kaliski <hi@szymonkaliski.com>
2024-04-15 18:09:49 +0200

    alfred has ai now

--- dotfiles/hammerspoon/bindings/global.lua
+++ dotfiles/hammerspoon/bindings/global.lua
@@ -72,53 +72,6 @@ local forceDrop = function()
   end
 end
 
-local toggleChatGPT = function()
-  if cache.chatGPTWebview == nil then
-    local focusedScreen = activeScreen()
-    local screenFrame = focusedScreen:frame()
-    local screenGrid = hs.grid.getGrid(focusedScreen)
-    local gridMargins = hs.grid.getMargins()
-
-    local approximateSize = {
-      h = 780,
-      w = 560
-    }
-
-    local size = {
-      w = math.ceil(approximateSize.w / screenGrid.w) * screenGrid.w - gridMargins[1] * 2,
-      h = math.ceil(approximateSize.h / screenGrid.h) * screenGrid.h - gridMargins[2] * 2
-    }
-
-    local topLeft = {
-      x = screenFrame.x + screenFrame.w / 2 - size.w / 2 + gridMargins[1],
-      y = screenFrame.y + screenFrame.h / 2 - size.h / 2 + gridMargins[2]
-    }
-
-    cache.chatGPTWebview = hs.webview.new()
-      :url('https://chat.openai.com')
-      :windowStyle(
-        hs.webview.windowMasks.titled |
-        hs.webview.windowMasks.resizable |
-        hs.webview.windowMasks.closable |
-        hs.webview.windowMasks.nonactivating |
-        hs.webview.windowMasks.utility |
-        hs.webview.windowMasks.HUD
-      )
-      :level(hs.drawing.windowLevels.floating)
-      :shadow(true)
-      :allowTextEntry(true)
-      :size(size)
-      :topLeft(topLeft)
-  end
-
-  if cache.chatGPTWebview:isVisible() then
-    cache.chatGPTWebview:hide()
-  else
-    cache.chatGPTWebview:show()
-  end
-end
-
-
 module.start = function()
   -- ultra bindings
   local ultra = { 'ctrl', 'alt', 'cmd' }
@@ -135,8 +88,6 @@ module.start = function()
     { key = 'd', fn = forceDrop                 },
     { key = 'v', fn = forcePaste                },
 
-    -- { key = 'i', fn = toggleChatGPT             },
-
     -- system toggles, moved to Alfred
     -- { key = 'b', fn = system.toggleBluetooth },
     -- { key = 's', fn = system.toggleDND       }, -- [s]ilence
@@ -151,7 +102,6 @@ module.start = function()
     { key = 'return', apps = config.apps.terms        },
     { key = 'space',  apps = config.apps.browsers     },
     { key = ',',      apps = { 'System Preferences' } },
-    { key = 'i',      apps = { 'Replit AI'          } }
   }, function(object)
     hs.hotkey.bind(ultra, object.key, function() smartLaunchOrFocus(object.apps) end)
   end)

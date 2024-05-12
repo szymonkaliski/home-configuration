49491ad50d5ad07366e8b305b134a00d75851278
Szymon Kaliski <hi@szymonkaliski.com>
2024-05-12 09:11:22 +0200

    launchctl generation script error handling

--- launchctls/generate-launchctl.sh
+++ launchctls/generate-launchctl.sh
@@ -4,7 +4,18 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 
 LAUNCHCTL_SCRIPTS_PATH="$(pwd)/scripts"
 
+if [ -z "$1" ]; then
+  echo "Usage: $0 TEMPLATE_FILE.tmpl"
+  exit 1
+fi
+
 TEMPLATE=$1
 OUTPUT="${TEMPLATE%.tmpl}"
 
-sed "s|LAUNCHCTL_SCRIPTS_PATH|$LAUNCHCTL_SCRIPTS_PATH|g" $TEMPLATE > $OUTPUT
+if [ -z "$OUTPUT" ]; then
+  echo "Could not generate the output file name"
+  exit 1
+fi
+
+sed "s|LAUNCHCTL_SCRIPTS_PATH|$LAUNCHCTL_SCRIPTS_PATH|g" "$TEMPLATE" > "$OUTPUT"
+

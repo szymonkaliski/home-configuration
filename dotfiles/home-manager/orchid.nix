c1b4f9adbb4dd9cb2582ba2578f8fcf237dda6a8
Szymon Kaliski <hi@szymonkaliski.com>
2025-11-04 19:34:34 +0100

    nix packages update

--- dotfiles/home-manager/orchid.nix
+++ dotfiles/home-manager/orchid.nix
@@ -35,10 +35,9 @@
     pkgs.nnn
     pkgs.nodejs_22
     pkgs.parallel
-    pkgs.poppler_utils # for `name-pdf-papers` script
+    pkgs.poppler-utils # for `name-pdf-papers` script
     pkgs.ripgrep
     pkgs.rsync
-    pkgs.terminal-notifier
     pkgs.tmux
     pkgs.tree
     pkgs.unixtools.watch

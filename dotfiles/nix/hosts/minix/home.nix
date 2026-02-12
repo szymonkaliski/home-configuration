7405598cb1c9da019cc79a567d77a2b2bf53a113
Szymon Kaliski <hi@szymonkaliski.com>
2026-02-12 19:33:00 +0100

    run lgtv2mqtt2 in systemd on minix

--- dotfiles/nix/hosts/minix/home.nix
+++ dotfiles/nix/hosts/minix/home.nix
@@ -14,6 +14,21 @@ let
     npmDepsHash = "sha256-TJfRdzvA8PybTRx8zUN+IgW819kZrz8ac3/wgJTi5Us=";
     dontNpmBuild = true;
   };
+
+  lgtv2mqtt2 = pkgs.buildNpmPackage {
+    pname = "lgtv2mqtt2";
+    version = "1.3.0";
+
+    src = pkgs.fetchFromGitHub {
+      owner = "szymonkaliski";
+      repo = "lgtv2mqtt2";
+      rev = "945e3e8566a35b2ede80bf4e1df2e03fb39e099a";
+      hash = "sha256-wuMnlHBTcfqsDfriErRwxIAsF0G+684/vWkGVqBzr6A=";
+    };
+
+    npmDepsHash = "sha256-bX0hcMUqPhqR5j6yNFdHom6TkSmMk4QXMdITaum6J+o=";
+    dontNpmBuild = true;
+  };
 in
 {
   imports = [ ../../common.nix ];
@@ -23,6 +38,7 @@ in
   home.packages = [
     neolink.packages.${pkgs.stdenv.hostPlatform.system}.default
     smartbox2mqtt
+    lgtv2mqtt2
     pkgs.lm_sensors
     pkgs.trash-cli
   ];
@@ -58,6 +74,24 @@ in
 
     Service = {
       ExecStart = "${smartbox2mqtt}/bin/smartbox2mqtt";
+      Restart = "on-failure";
+      RestartSec = 10;
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
+
+  systemd.user.services.lgtv2mqtt2 = {
+    Unit = {
+      Description = "LG TV to MQTT bridge";
+      After = [ "network-online.target" ];
+      Wants = [ "network-online.target" ];
+    };
+
+    Service = {
+      ExecStart = "${lgtv2mqtt2}/bin/lgtv2mqtt2";
       Restart = "on-failure";
       RestartSec = 10;
     };

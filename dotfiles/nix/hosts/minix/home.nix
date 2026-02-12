c028f3f116ab1b83bc7d438a3c2ce1b6425d280a
Szymon Kaliski <hi@szymonkaliski.com>
2026-02-12 10:03:51 +0100

    run neolink in systemd on minix

--- dotfiles/nix/hosts/minix/home.nix
+++ dotfiles/nix/hosts/minix/home.nix
@@ -11,4 +11,24 @@
   ];
 
   services.dropbox.enable = true;
+
+  systemd.user.services.neolink = {
+    Unit = {
+      Description = "Neolink MQTT bridge for Reolink cameras";
+      After = [ "network-online.target" ];
+      Wants = [ "network-online.target" ];
+    };
+
+    Service = {
+      ExecStart = "${
+        neolink.packages.${pkgs.stdenv.hostPlatform.system}.default
+      }/bin/neolink mqtt --config=%h/.config/neolink/config.toml";
+      Restart = "on-failure";
+      RestartSec = 10;
+    };
+
+    Install = {
+      WantedBy = [ "default.target" ];
+    };
+  };
 }

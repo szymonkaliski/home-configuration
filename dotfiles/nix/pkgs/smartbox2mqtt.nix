d4bfe6866dd4c69563b2f59d987b591acb024415
Szymon Kaliski <hi@szymonkaliski.com>
2026-02-22 19:39:10 +0100

    update smartbox2mqtt

--- dotfiles/nix/pkgs/smartbox2mqtt.nix
+++ dotfiles/nix/pkgs/smartbox2mqtt.nix
@@ -9,8 +9,8 @@ buildNpmPackage {
   src = fetchFromGitHub {
     owner = "szymonkaliski";
     repo = "smartbox2mqtt";
-    rev = "2a16c8281a531c3f49ca20ea77a2cc8cc7a84163";
-    hash = "sha256-4uJYxlARLDHooFiHxVYLMghAYa5qSqqddFqgu1xc2Ow=";
+    rev = "6ec4ebc13108661fdddb1655e0d28dd224e0d932";
+    hash = "sha256-/PyKqHYzyloaHFUBWYQaYCdoCO+Jc2C0hHpTBQ2GQ7k=";
   };
 
   npmDepsHash = "sha256-TJfRdzvA8PybTRx8zUN+IgW819kZrz8ac3/wgJTi5Us=";

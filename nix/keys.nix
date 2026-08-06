e739277ecde36f5524f92914aaa5367b23b4be22
Szymon Kaliski <hi@szymonkaliski.com>
2026-08-06 13:23:02 +0200

    keys: rotate berry's user and host keys for the new sd card

--- nix/keys.nix
+++ nix/keys.nix
@@ -2,7 +2,7 @@
 {
   orchid = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKcLnVCHFI88PfD5qNEjXkerjFLR64LGkixwafkaW4m7 szymon@orchid";
   minix = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJUJt+pQbfy7QwY8EieP5EmX1suXdt9bDECsokG6x/3L szymon@minix";
-  berry = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAs/X3At6YhJG9tQGuwJc9PfDdMMkv383zkYWKMtMGeY szymon@berry";
+  berry = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIPujiRSiTG7nVWeBsBYuINJPLsJgyIsz69f1a+KiPlK szymon@berry";
 
   # berry's nix-daemon, for offloading builds to minix
   # private half in secrets/nix-builder-key
@@ -10,5 +10,5 @@
   # host key, not a user key - pins minix in berry's known_hosts
   minixHost = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPHSvjTQM8nV2f3LyMW6H4iuORbvPi+lFRkq2XeGi+VJ root@minix";
   # host key, not a user key - pins berry in minix's known_hosts
-  berryHost = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJlyh3a/IKM22PD9e89/ZllxqehHe9Y8lIwyaWaQt1ZL root@berry";
+  berryHost = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMmx4IzDKUDlKY4lN+88uiJKrNlqNwcw6oc69iasMtf/ root@berry";
 }
